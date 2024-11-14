23e3e007809c924730bc6f27bf33177ac888f5d1
Michael Brady <michaelbrady712@gmail.com>
2024-11-14 17:58:47 -0600

    Add project files.

---
+++ network_monitor/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace network_monitor.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
